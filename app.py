a243eacc598b2455d1a8c48447f545eeb0aafb50
Farhan <farhan0904@gmail.com>
2020-07-23 17:36:55 +0000

    Finished setting up mongo and made base.html

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
-
-
+from flask import render_template
+from flask import request
+from flask import url_for
+from flask import redirect
+from flask import request
+from flask_pymongo import PyMongo
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import datetime
 # -- Initialization section --
+
 app = Flask(__name__)
+app.jinja_env.globals['current_time'] = datetime.datetime.now()
+# -- Initialization section --
+
+
+MONGO_DBNAME = os.getenv("MONGO_DBNAME")
+MONGO_DB_USERNAME = os.getenv("MONGO_DB_USERNAME")
+MONGO_DB_PASSWORD = os.getenv("MONGO_DB_PASSWORD")
+#
+app.config['MONGO_DBNAME'] = MONGO_DBNAME
+app.config['MONGO_URI'] = f'mongodb+srv://{MONGO_DB_USERNAME}:{MONGO_DB_PASSWORD}@user-game-data.yi30k.mongodb.net/{MONGO_DBNAME}?retryWrites=true&w=majority'
+mongo = PyMongo(app)
 
 
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template('index.html')
